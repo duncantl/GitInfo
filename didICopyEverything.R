6c370a9a283bfcd1a946c4b5e958e8041a7351d5
Duncan Temple Lang <duncan@r-project.org>
2025-02-28 06:43:12 -0800

    description of what this is for.

--- didICopyEverything.R
+++ didICopyEverything.R
@@ -1,21 +1,18 @@
-# 
-paths = c(".", "~/OGS/GradGroupAnalysis")
-npaths = normalizePath(paths)
+# compare 2 directories - for use after we extract a directory from a larger git repos. into
+# its own repos but have untracked files in the original and move them over. This checks what we haven't copied yet.
 
-#newDir = "."
-#oldDir = normalizePath()
+cmpDirs = 
+function(new = ".", old = "~/OGS/GradGroupAnalysis")
+{
+    paths = c(new, old)
+    npaths = normalizePath(paths)
 
-files = lapply(paths, list.files, recursive = TRUE, include.dirs = TRUE, all = TRUE, full.names = TRUE)
+    files = lapply(paths, list.files, recursive = TRUE, include.dirs = TRUE, all = TRUE, full.names = TRUE)
 
-files2 = mapply(function(x, p) gsub( paste0("^", p, "/"), "", x),  files, npaths, SIMPLIFY = FALSE)
+    files2 = mapply(function(x, p) gsub( paste0("^", p, "/"), "", x),  files, npaths, SIMPLIFY = FALSE)
 
-files2[[1]] = files2[[1]][ - grep("\\.git/", files2[[1]] ) ]
+    files2[[1]] = files2[[1]][ - grep("\\.git/", files2[[1]] ) ]
 
-do.call(setdiff, files2)
-
-#new2 = new[ - grep("\\.git/", new) ]
-#old2 = gsub("^/Users/duncan/OGS/GradGroupAnalysis/", "", old)
-#new3 = gsub("^\\./", "", new2)
-
-setdiff(new3, old2)
-setdiff(old2, new3)
+    list(onlyInNew = do.call(setdiff, files2),
+         notCopiedFromOld = do.call(setdiff, rev(files2)))
+}
