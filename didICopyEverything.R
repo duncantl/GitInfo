bcf06fe82fe6c778f8a371294ae08415ca08da38
Duncan Temple Lang <duncan@r-project.org>
2025-02-28 06:54:50 -0800

    added rmTilde parameter and code

--- didICopyEverything.R
+++ didICopyEverything.R
@@ -2,7 +2,7 @@
 # its own repos but have untracked files in the original and move them over. This checks what we haven't copied yet.
 
 cmpDirs = 
-function(new = ".", old = "~/OGS/GradGroupAnalysis")
+function(new = ".", old = "~/OGS/GradGroupAnalysis", rmTilde = TRUE)
 {
     paths = c(new, old)
     npaths = normalizePath(paths)
@@ -13,6 +13,9 @@ function(new = ".", old = "~/OGS/GradGroupAnalysis")
                     files, npaths, SIMPLIFY = FALSE)
 
     files2[[1]] = files2[[1]][ - grep("\\.git/", files2[[1]] ) ]
+
+    if(rmTilde)
+        files2 = lapply(files2, function(x) x[ !grepl("~$", x) ] )
 
     list(onlyInNew = do.call(setdiff, files2),
          notCopiedFromOld = do.call(setdiff, rev(files2)))
